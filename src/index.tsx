648ddc1625e40ee977f34971ade0ab21b74d5f36
masashi morimoto <masashi.morimoto.vd@ambl.co.jp>
2022-12-23 22:51:12 +0900

    add persist

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SWRConfig } from 'swr';
+import { localStorageProvider } from './lib';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SWRConfig 
+      value={{ provider: localStorageProvider }}
+    >
+      <App />
+    </SWRConfig>
   </React.StrictMode>,
   document.getElementById('root')
 );
